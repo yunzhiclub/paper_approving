6aa5e0288e052ab8d67adb0ce66dad7d2994fad7
denghaoyang <275111108@qq.com>
2016-01-12 20:04:47 +0800

    引入PHPExcel

---
+++ Application/Admin/Controller/StudentController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author denghaoyang
+ */
+
+namespace Admin\Controller;
+
+use Admin\Controller\AdminController;
+
+class StudentController extends AdminController {
+	public function indexAction(){
+		
+	}
+}
+    
