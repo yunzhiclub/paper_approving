3a517938e820249c18fcfc6bcb3f72615ab48152
anqiang1995 <anqiang1995@qq.com>
2016-01-13 17:37:15 +0800

    显示数据存数据

--- Application/Expert/Logic/ExpertLogic.class.php
+++ Application/Expert/Logic/ExpertLogic.class.php
@@ -5,5 +5,27 @@ use Expert\Model\ExpertModel;
 
 class ExpertLogic extends ExpertModel
 {
-	
+	protected $errors = array();   			//错误信息
+
+	public function addList($list)			//添加活动
+	{
+		try{
+			if($this->create($list))
+			{
+				$id=$this->save();
+				return $id; 
+			}
+			else
+			{
+				$this->errors[]=$this->getError();
+				return false;
+			}
+		}
+		catch(\Think\Exception $e)
+		{
+			$this->errors[]=$e->getMessage();
+			return false;
+		}
+	}
 }
+
--- Application/Home/Controller/ExpertController.class.php
+++ Application/Home/Controller/ExpertController.class.php
@@ -2,15 +2,39 @@
 namespace Home\Controller;
 
 use Think\Controller;
+use Expert\Logic\ExpertLogic;
 
 class ExpertController extends Controller
 {
 	public function editAction()
 	{
+		//实例化对象调信息
+		$ExpertL = new ExpertLogic();
+		$expert=$ExpertL->getlists();
+		//向V层赋值
+		$this->assign('expert',$expert[0]);
 		$this->display();
 	}
 	public function saveAction()
 	{
-		
+		//获取专家个人信息
+		$expert=I('post.');
+
+		//保存专家个人信息
+		$ExpertL = new ExpertLogic();
+		$ExpertL->addList($expert);
+		//判断
+		if(count($errors=$ExpertL->getErrors())!==0)
+        {
+            //数组变字符串
+            $error =implode('<br/>', $errors);
+            
+            
+            //显示错误
+             $this->error("添加失败，原因：".$error,U('Home/Expert/edit?p='.I('get.p')));
+            
+        }
+        $this->success("操作成功" , U('Home/Expert/edit?p='.I('get.p'))); 
 	}
+	
 }
