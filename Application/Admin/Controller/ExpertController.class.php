0b6fd377a250efd231665004eb802c5a0c2f7eb3
anqiang1995 <anqiang1995@qq.com>
2016-01-26 18:21:35 +0800

    重置专家密码

--- Application/Admin/Controller/ExpertController.class.php
+++ Application/Admin/Controller/ExpertController.class.php
@@ -7,24 +7,40 @@ use Review\Logic\ReviewLogic;
 class ExpertController extends AdminController
 {
 
-	public function indexAction(){
+	public function indexAction()				//初始页面
+	{
 
-		$ReviewL=new ReviewLogic();
+		$ReviewL=new ReviewLogic();				
 
-		$reviews=$ReviewL->getlists();
+		$reviews=$ReviewL->getlists();			//取评阅信息
 
-		$this->assign('reviews',$reviews);
+		$this->assign('reviews',$reviews);		//向V层赋值
 
 		$this->display();
 		
 	}
 
-	public function resetAction()
+	public function resetAction()				//重置密码
 	{
 		$ReviewL=new ExpertLogic();
 
-		$id=I('get.expert_id')
+		$id=I('get.expert_id')					//取对应专家id
 
-		$review=$ReviewL->resetPasswordByExpert_id($id);
+		$review=$ReviewL->resetPasswordByExpert_id($id);	//重置该专家密码
+
+		if(count($errors=$ReviewL->getErrors())!==0)
+		{
+            //数组变字符串
+			$error =implode('<br/>', $errors);
+			
+			
+            //显示错误
+			$this->error("添加失败，原因：".$error,U('index?id=', I('get')));
+
+			return false;
+			
+		}
+		$this->success("操作成功" , U('index?id=', I('get')));
 	}
+}
 }
--- Application/Review/Logic/ReviewLogic.class.php
+++ Application/Review/Logic/ReviewLogic.class.php
@@ -2,11 +2,35 @@
 namespace Review\logic;
 
 use Review\Model\ReviewModel;
+use Expert\Logic\ExpertLogic;
 
 class ReviewLogic extends ReviewModel
 {
+	protected $errors = array();   							//错误信息
+
 	public function resetPasswordByExpert_id($id)
 	{
-		
+		$expertL = new $expertLogic();
+		$expert = $expertL->getListById($id);					//取对应专家信息
+		$expert['userpassword'] = 123456;						//重置对应专家密码
+		//存储
+		try
+		{
+			if($this->create($expert))
+			{
+				$id = $this->save();
+				return $id; 
+			}
+			else
+			{
+				$this->errors[] = $this->getError();
+				return false;
+			}
+		}
+		catch(\Think\Exception $e)
+		{
+			$this->errors[] = $e->getMessage();
+			return false;
+		}
 	}
 }
