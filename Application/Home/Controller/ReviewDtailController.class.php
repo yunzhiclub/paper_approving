606b8fb69b610f327a18471a520ee11665e2d778
weijingyun <1028193951@qq.com>
2016-02-22 20:29:34 +0800

    修改

--- Application/Home/Controller/ReviewDtailController.class.php
+++ Application/Home/Controller/ReviewDtailController.class.php
@@ -8,14 +8,14 @@ class ReviewDetailController extends HomeController
     //用户列表显示
     public function indexAction()
     {
-        //获取列表
-        $ReviewDetailL = new ReviewDetailLogic();
-        $ReviewDetails = $ReviewDetailL->getLists();
-        
-        //传入列表
-        $this -> assign('ReviewDetails',$ReviewDetails);
+        // //获取列表
+        // $ReviewDetailL = new ReviewDetailLogic();
+        // $ReviewDetails = $ReviewDetailL->getLists();
 
-        //调用v层
+        // //传入列表
+        // $this -> assign('ReviewDetails',$ReviewDetails);
+
+        // //调用v层ss
         $this -> display();
     }
 }
