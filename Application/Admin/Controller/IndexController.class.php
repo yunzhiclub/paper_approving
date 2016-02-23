bf858c6377e884825c55a32a70ffe768e2458905
mskies <3792535@qq.com>
2016-02-23 18:33:33 +0800

    增加phpexcel数据转化成文件示例代码

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -6,17 +6,22 @@ class IndexController extends AdminController {
     
     public function indexAction(){
         
-        $this->display();
-        $filePath = "/Applications/XAMPP/htdocs/paper_approving/Application/Test/yunzhi.xlsx";
+        // $this->display();
+        // //读取execl文件，并将结果做为数组返回
+        // $filePath = "/Applications/XAMPP/htdocs/paper_approving/Application/Test/yunzhi.xlsx";
         $ReadL = new PHPExcelLogic;
-        if (!$data = $ReadL->ReadFile($filePath))
-        {
-            dump($ReadL->getError());
-        }
-        else
-        {
-            dump($data);
-        }
+        // if (!$data = $ReadL->ReadFile($filePath))
+        // {
+        //     dump($ReadL->getError());
+        // }
+        // else
+        // {
+        //     dump($data);
+        // }
+
+        // 将数组中的值写入EXCEL，供用户下载
+        $ReadL->arrayToExcel();
+
     }
 
     public function saveAction(){
--- Application/Yunzhi/Logic/PHPExcelLogic.class.php
+++ Application/Yunzhi/Logic/PHPExcelLogic.class.php
@@ -14,6 +14,7 @@ namespace Yunzhi\Logic;
 
 Vendor('PHPExcel.PHPExcel.IOFactory');
 Vendor('PHPExcel.PHPExcel.Cell');
+Vendor('PHPExcel.PHPExcel');
 
 class PHPExcelLogic
 {
@@ -87,5 +88,96 @@ class PHPExcelLogic
             $this->setError("ReadL: exceptions " . $e->getMessage());
             return false;
         }
+    }
+
+
+    /**
+     * 本程序给出使用PHPExcel将数组中的数据转化为EXCEL文件的示例
+     * 程序的目的在于展示 使用PHPExcel将数组变成excel 的方法。
+     * 起到示例代码的作用
+     * @return file 
+     */
+    public function arrayToExcel()
+    {
+        //未例数据
+        $data = array(
+            array('name' => 'NAME', 'mail' => 'EMAIL', 'age' => 'age'),
+            array('name' => 'A', 'mail' => 'a@gmail.com', 'age' => 43),
+            array('name' => 'C', 'mail' => 'c@gmail.com', 'age' => 24),
+            array('name' => 'B', 'mail' => 'b@gmail.com', 'age' => 35),
+            array('name' => 'G', 'mail' => 'f@gmail.com', 'age' => 22),
+            array('name' => 'F', 'mail' => 'd@gmail.com', 'age' => 52),
+            array('name' => 'D', 'mail' => 'g@gmail.com', 'age' => 32),
+            array('name' => 'E', 'mail' => 'e@gmail.com', 'age' => 34),
+            array('name' => 'K', 'mail' => 'j@gmail.com', 'age' => 18),
+            array('name' => 'L', 'mail' => 'h@gmail.com', 'age' => 25),
+            array('name' => 'H', 'mail' => 'i@gmail.com', 'age' => 28),
+            array('name' => 'J', 'mail' => 'j@gmail.com', 'age' => 53),
+            array('name' => 'I', 'mail' => 'l@gmail.com', 'age' => 26),
+        );
+
+        //实例化对像，注意，在NEW之前，在确定已经在文件头部进行了 
+        //Vendor('PHPExcel.PHPExcel');
+        //实例化时，要在类前加一个 \
+        $objPHPExcel = new \PHPExcel();
+        
+        // 使用内类的方法，从A1开始，将数组中的数据今次添加至工作表
+        $objPHPExcel->getActiveSheet()->fromArray($data, null, 'A1');
+        
+        // 重新命名工作表
+        $objPHPExcel->getActiveSheet()->setTitle('云智PHPExcel填充示例');
+        
+        // 将A，B两列宽度设置为自动
+        $objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+        $this->_download($objPHPExcel);
+    }
+
+    private function _save($objPHPExcel)
+    {   
+        //文件路径可以为绝对路径，也可以为相对路径。相对路径，请注意自己的当前入口位置
+        $filePath = '/Applications/XAMPP/xamppfiles/htdocs/paper_approving/Public/download/';
+        $fileName = '云智-数组存为EXCEL' . date("Ymdhis") . '.xls';
+
+        // 设置文件格式为Excel2007或excel2003。
+        // 同样，在使用phpexcel内置内时，需要注意两点：
+        // 1.vender了该类所在的文件
+        // 2.使用时，在类前面加入 \ 
+        // （由于createWriter为PHPExcel_IOFactory的静态方法，所以可以不必实例化而直接使用）
+        // $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+
+        //以下为存为2003及以前的格式
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+
+        //存入指定路径
+        $objWriter->save($filePath . $fileName);
+        
+    }
+
+    private function _download($objPHPExcel)
+    {   
+        $fileName = '云智-数组存为EXCEL' . date("Ymdhis") . '.xls';
+        /*引导用户直接下载需要进行两步操作,注意一点。
+        * 1.重写header信息，告知浏览器用户将要下载的文件类型，默认的文件名等信息
+        * 2.调用phpexcel供用户下载方法
+        * 注意1点：由于在这里重写了header信息，所以，在下载的页面，是不宜继承我们的其它的模板的，否则可以会导致重写header引发的错误.
+        * */
+        // excel2007 content-type
+        // header('Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+        //excel 2003 content-type
+        // Redirect output to a client’s web browser (Excel5)
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'. $fileName . '"');
+        header('Cache-Control: max-age=0');
+
+        //以下存为2007格式
+        // $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+
+        //以下为存为2003及以前的格式
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        
+        // 引导用户下载
+        $objWriter->save('php://output');
     }
 }
