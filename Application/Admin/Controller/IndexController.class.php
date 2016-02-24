75178c40fc88046ac36bc980215dfed1ccb7a2be
mskies <3792535@qq.com>
2016-02-24 12:37:23 +0800

    增加单元测试

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -2,29 +2,12 @@
 namespace Admin\Controller;
 use Admin\Controller;
 use Yunzhi\Logic\PHPExcelLogic;
+use Yunzhi\Logic\ZipLogic;
+
 class IndexController extends AdminController {
     
     public function indexAction(){
         
-        // $this->display();
-        // //读取execl文件，并将结果做为数组返回
-        // $filePath = "/Applications/XAMPP/htdocs/paper_approving/Application/Test/yunzhi.xlsx";
-        $ReadL = new PHPExcelLogic;
-        // if (!$data = $ReadL->ReadFile($filePath))
-        // {
-        //     dump($ReadL->getError());
-        // }
-        // else
-        // {
-        //     dump($data);
-        // }
-
-        // 将数组中的值写入EXCEL，供用户下载
-        $ReadL->arrayToExcel();
-
-    }
-
-    public function saveAction(){
-    	dump(I('post.'));
+        $this->display();
     }
 }
