c0d71be5585a9fa4a7408c094bebfbfe32c8253a
mskies <3792535@qq.com>
2016-03-14 12:27:47 +0800

    去除冗余信息

--- Application/Student/Model/StudentModel.class.php
+++ Application/Student/Model/StudentModel.class.php
@@ -7,8 +7,5 @@ use Yunzhi\Model\YunzhiModel;
 class StudentModel extends YunzhiModel
 {
     protected $orderBys     = array("student_no"=>"asc"); //排序字段方式
-    protected $keywords     = "I('get.keywords')"; //查询关键字
     protected $field        = "student_no"; //查询字段
-
-
 }
