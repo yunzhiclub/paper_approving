adbc546853706524f060c381288b107425fc5451
panjie <3792535@qq.com>
2018-03-15 09:03:12 +0800

    修改配置文件。开启测试模式

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -23,8 +23,8 @@ return array(
     'DB_TYPE'               =>  'mysqli',     // 数据库类型
     'DB_HOST'               =>  'localhost',    // 服务器地址
     'DB_NAME'               =>  'paper_approving',          // 数据库名
-    'DB_USER'               =>  'paper_approving',      // 用户名
-    'DB_PWD'                =>  'HKDLr9hcFYZaXe3AUyILizr5cSfCX2',          // 密码
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '3306',             // 端口
     'DB_PREFIX'             =>  'paper_',           // 数据库表前缀
 
--- Public/admin.php
+++ Public/admin.php
@@ -12,19 +12,19 @@
 // 应用入口文件
 
 // 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+if (version_compare(PHP_VERSION, '5.3.0', '<')) die('require PHP > 5.3.0 !');
 
 //场景设置
-define('APP_STATUS','admin');
+define('APP_STATUS', 'admin');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',false);
+define('APP_DEBUG', true);
 
 //定义Runtime 临时文件夹
-define('RUNTIME_PATH','./../Runtime/');
+define('RUNTIME_PATH', './../Runtime/');
 
 // 定义应用目录
-define('APP_PATH','./../Application/');
+define('APP_PATH', './../Application/');
 
 // 引入ThinkPHP入口文件
 require './../ThinkPHP/ThinkPHP.php';
