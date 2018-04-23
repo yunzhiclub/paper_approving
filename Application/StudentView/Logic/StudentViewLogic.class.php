1ac6d1a2d5aaa7cab96e5070f5e4a65f216eb8a8
panjie <3792535@qq.com>
2018-04-23 12:48:09 +0800

    移动文件夹

--- Application/StudentView/Logic/StudentViewLogic.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace StudentView\Logic;
-
-use StudentView\Model\StudentViewModel;
-
-class StudentViewLogic extends StudentViewModel
-{
-
-}
--- Application/StudentView/Model/StudentViewModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace StudentView\Model;
-
-use Yunzhi\Model\YunzhiModel;
-
-class StudentViewModel extends YunzhiModel
-{
-    protected $field = "student_no";    //设置查询字段
-}
